8e02d8f8a868e523b92eafa8bc12468649a191bf
Joey Lim <jyorin0206@gmail.com>
2022-08-24 14:44:27 +0800

    made pet walk forward and backward

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,46 @@
 <script lang="ts">
   import { window } from "@tauri-apps/api";
-  import { LogicalPosition, LogicalSize, PhysicalSize, type Monitor } from "@tauri-apps/api/window";
+  import {
+    LogicalPosition,
+    LogicalSize,
+    PhysicalSize,
+    type Monitor,
+  } from "@tauri-apps/api/window";
   import { onMount } from "svelte";
   import Pet from "./lib/Pet.svelte";
 
   const interval = 350;
+  const movementSpeed = 4
   let positionX = 0;
   let positionY = 0;
-  // setInterval(() => {
-  //         positionX += 4;
-  //         if (positionX >= size.width) positionX = 0;
-  //         window.appWindow.setPosition(
-  //           new LogicalPosition(positionX, positionY)
-  //         );
-  //       }, interval);
+  let isBackward = false;
+
   onMount(() => {
     moveWindow();
   });
   async function moveWindow() {
     const monitors: Monitor[] = await window.availableMonitors();
-    const monitor = monitors[0]
-    const scaleFactor = monitor.scaleFactor
-    const size: PhysicalSize = monitor.size
-    const logicalSize : LogicalSize = new LogicalSize(size.width / scaleFactor, size.height / scaleFactor)
+    const monitor = monitors[0];
+    const scaleFactor = monitor.scaleFactor;
+    const size: PhysicalSize = monitor.size;
+    const logicalSize: LogicalSize = new LogicalSize(
+      size.width / scaleFactor,
+      size.height / scaleFactor
+    );
     setInterval(() => {
-      if (positionX >= logicalSize.width) positionX = 0;
-      positionX += 4;      
+      if (positionX >= logicalSize.width) {
+        isBackward = true;
+      } else if (positionX == 0) {
+        isBackward = false;
+      }
+      if (isBackward) positionX -= movementSpeed; else positionX += movementSpeed;
       window.appWindow.setPosition(new LogicalPosition(positionX, positionY));
     }, interval);
   }
 </script>
 
 <main>
-  <Pet />
+  <Pet {isBackward} />
 </main>
 
 <style>
--- src/lib/Pet.svelte
+++ src/lib/Pet.svelte
@@ -1,20 +1,29 @@
 <script>
-    let backgroundX = 0
-    let backgroundY = -32
-    const interval = 200
-    // document.getElementById("sprite").style.backgroundPosition = `${backgroundX}px ${backgroundY}px`
-    function animateSprite() {
-        backgroundX-=32
-        // console.log(backgroundX, backgroundY)
-        // if (backgroundX == -128) backgroundX = 0
-    }
-    setInterval(() => {
-        animateSprite()
-    }, interval)
+  export let isBackward = false;
+  let backgroundX = 0;
+  let backgroundY = -32;
+  const interval = 200;
+  function moveForward() {
+    backgroundX -= 32;
+    backgroundY = -32;
+  }
+
+  function moveBackward() {
+    backgroundX -= 32;
+    backgroundY = -96;
+  }
+  setInterval(() => {
+    if (isBackward) moveBackward();
+    else moveForward();
+    // animateSprite()
+  }, interval);
 </script>
 
 <div id="pet">
-    <p id="sprite" style="background: url('src/assets/stardew_cat_spritesheet.png') {backgroundX.toString()}px {backgroundY.toString()}px; width: 32px; height: 32px;"></p>
+  <p
+    id="sprite"
+    style="background: url('src/assets/stardew_cat_spritesheet.png') {backgroundX.toString()}px {backgroundY.toString()}px; width: 32px; height: 32px;"
+  />
 </div>
 
 <style>
